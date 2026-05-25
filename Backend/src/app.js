2f2bef7d0d72c2d0b3ec780a8a8bec0f44032e76
github practice <karanhingrajiya69@gmail.com>
2026-05-25 16:01:59 +0530

    fixed rendering error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "https://pollpulse-backend-rusz.onrender.com",
     credentials: true,
   })
 );
